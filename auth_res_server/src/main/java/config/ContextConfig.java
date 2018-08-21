4ac88a7d31b82fc9a30e13f05131f21991a6abf0
mr_tompkins <mr_tompkins@PC>
2018-08-21 10:47:39 +0200

    Add sata source configuration

--- auth_res_server/src/main/java/config/ContextConfig.java
+++ auth_res_server/src/main/java/config/ContextConfig.java
@@ -1,9 +1,27 @@
 package config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:/resource_server_properties.properties")
 public class ContextConfig {
 
+    @Value("${dataSourceUrl}") String url;
+    @Value("${dataSourcePassword") String password;
+    @Value("${dataSourceUsername") String username;
 
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setPassword(password);
+        basicDataSource.setUsername(username);
+        basicDataSource.setUrl(url);
+        return basicDataSource;
+    }
 }
