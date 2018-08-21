b5ef3cb1bf40a52df4ea3988ce87bc82fe400bcf
mr_tompkins <mr_tompkins@PC>
2018-08-21 17:56:00 +0200

    Make Context and test to load properties via EnvPropertiesLoader

--- auth_res_server/src/main/java/config/ContextConfig.java
+++ auth_res_server/src/main/java/config/ContextConfig.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import utils.EnvPropertiesLoader;
+import utils.LoadedEnvProperties;
 
 import javax.sql.DataSource;
 
@@ -12,16 +15,18 @@ import javax.sql.DataSource;
 @PropertySource("classpath:/resource_server_properties.properties")
 public class ContextConfig {
 
-    @Value("${dataSourceUrl}") String url;
-    @Value("${dataSourcePassword}") String password;
-    @Value("${dataSourceUsername}") String username;
+    @Bean
+    public LoadedEnvProperties loadedEnvProperties(final Environment env) {
+        EnvPropertiesLoader envPropertiesLoader = new EnvPropertiesLoader(env);
+        return envPropertiesLoader.load();
+    }
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(final LoadedEnvProperties properties) {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setPassword(password);
-        basicDataSource.setUsername(username);
-        basicDataSource.setUrl(url);
+        basicDataSource.setPassword(properties.dataSourcePassword);
+        basicDataSource.setUsername(properties.dataSourceUsername);
+        basicDataSource.setUrl(properties.dataSourceURL);
         return basicDataSource;
     }
 }
--- auth_res_server/src/test/java/config/ConfigTest.java
+++ auth_res_server/src/test/java/config/ConfigTest.java
@@ -1,13 +1,18 @@
 package config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
 
 import static org.hamcrest.CoreMatchers.is;
 
@@ -16,14 +21,17 @@ import static org.hamcrest.CoreMatchers.is;
 @RunWith(SpringRunner.class)
 public class ConfigTest {
 
-    @Value("${dataSourceUrl}") String url;
-    @Value("${dataSourcePassword}") String password;
-    @Value("${dataSourceUsername}") String username;
+    private final ApplicationContext ctx;
+
+    public ConfigTest(final ApplicationContext ctx) {
+        this.ctx = ctx;
+    }
 
     @Test
     public void dataSourceIsProperlyInitialized() {
-        Assert.assertThat(password, is("\"\""));
-        Assert.assertThat(url, is("\"jdbc:mysql://localhost:3306/Book_library?serverTimezone=UTC\""));
-        Assert.assertThat(username, is("\"root\""));
+        BasicDataSource dataSource = ctx.getBean("dataSource", BasicDataSource.class);
+        Assert.assertThat(dataSource.getPassword(), is("\"\""));
+        Assert.assertThat(dataSource.getUrl(), is("\"jdbc:mysql://localhost:3306/Book_library?serverTimezone=UTC\""));
+        Assert.assertThat(dataSource.getUsername(), is("\"root\""));
     }
 }
