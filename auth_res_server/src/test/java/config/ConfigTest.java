121b924f4043208299512c0619e32125c9eecc71
mr_tompkins <mr_tompkins@PC>
2018-08-21 18:35:15 +0200

    Refactor ContextTest

--- auth_res_server/src/test/java/config/ConfigTest.java
+++ auth_res_server/src/test/java/config/ConfigTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
@@ -14,24 +15,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
 
+import static config.ConfigTest.DataSourceConstants.*;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 @TestPropertySource("classpath:/resource_server_properties.properties")
 @ContextConfiguration(classes = ContextConfig.class)
 @RunWith(SpringRunner.class)
 public class ConfigTest {
 
-    private final ApplicationContext ctx;
-
-    public ConfigTest(final ApplicationContext ctx) {
-        this.ctx = ctx;
-    }
+    @Autowired
+    private ApplicationContext ctx;
 
     @Test
     public void dataSourceIsProperlyInitialized() {
         BasicDataSource dataSource = ctx.getBean("dataSource", BasicDataSource.class);
-        Assert.assertThat(dataSource.getPassword(), is("\"\""));
-        Assert.assertThat(dataSource.getUrl(), is("\"jdbc:mysql://localhost:3306/Book_library?serverTimezone=UTC\""));
-        Assert.assertThat(dataSource.getUsername(), is("\"root\""));
+        assertThat(dataSource.getPassword(), is(expectedDataSourcePassword));
+        assertThat(dataSource.getUrl(), is(expectedDataSourceUrl));
+        assertThat(dataSource.getUsername(), is(getExpectedDataSourceUsername));
+    }
+
+    public static class DataSourceConstants {
+        public static final String expectedDataSourceUrl = "\"jdbc:mysql://localhost:3306/Book_library?serverTimezone=UTC\"";
+        public static final String expectedDataSourcePassword = "\"\"";
+        public static final String getExpectedDataSourceUsername = "\"root\"";
     }
 }
